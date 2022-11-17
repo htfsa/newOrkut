51629b5ecd9e8749571206021677eeab8a8012f0
Herbert Ferreira da Silva azevedo <112709078+kakarotofla@users.noreply.github.com>
2022-11-17 14:45:08 -0300

    INTRODUCTION DOM

---
+++ changephotos.js
@@ -0,0 +1,7 @@
+function handleRight (){
+    document.querySelector('.photo').src = "https://i.imgur.com/GoOW8yn.jpeg"
+    }
+function handleLeft(){
+    document.querySelector('.photo').src = "./hbt.jpg";
+
+    }
