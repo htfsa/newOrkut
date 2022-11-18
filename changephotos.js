955857dddeff0e73c63021bcd01157866091716a
Herbert Ferreira da Silva azevedo <112709078+kakarotofla@users.noreply.github.com>
2022-11-18 08:56:57 -0300

    INTRODUCTION DOM

--- changephotos.js
+++ changephotos.js
@@ -1,7 +1,27 @@
-function handleRight (){
-    document.querySelector('.photo').src = "https://i.imgur.com/GoOW8yn.jpeg"
-    }
-function handleLeft(){
-    document.querySelector('.photo').src = "./hbt.jpg";
 
+function handleLeft(){
+    document.querySelector('.photo').src = "photos/hbt.jpg";
+}
+
+
+
+const imagens = ['photos/photo1.jpg',
+                'photos/photo2.jpg',
+                'photos/photo3.jpg',
+                'https://i.imgur.com/GoOW8yn.jpeg'
+                ];
+
+let photoatual = 0
+
+function pulo(){
+    photoatual++;
+    if (photoatual>= imagens.length){
+        photoatual=0
     }
+
+
+
+document.querySelector('#foto').src = imagens[photoatual];
+}
+
+
